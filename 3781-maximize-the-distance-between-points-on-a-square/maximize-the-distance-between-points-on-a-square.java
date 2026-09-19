f73ff8961575c0fb460a5058fa9f11220c7aae61
Anonymous <kaartikchhajer@gmail.com>
2026-09-20 01:06:58 +0530

    Time: 21 ms (71.56%) | Memory: 52 MB (29.78%) - LeetSync

---
+++ 3781-maximize-the-distance-between-points-on-a-square/maximize-the-distance-between-points-on-a-square.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+class Solution {
+    public int maxDistance(int side, int[][] points, int k) {
+        int n = points.length;
+        long perimeter = 4L * side;
+
+        long[] pos = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            int x = points[i][0];
+            int y = points[i][1];
+
+            if (y == 0) {
+                pos[i] = x;
+            } else if (x == side) {
+                pos[i] = (long) side + y;
+            } else if (y == side) {
+                pos[i] = 3L * side - x;
+            } else {
+                pos[i] = 4L * side - y;
+            }
+        }
+
+        Arrays.sort(pos);
+
+        long low = 0;
+        long high = 2L * side;
+
+        while (low < high) {
+            long mid = low + (high - low + 1) / 2;
+
+            if (canPlace(pos, perimeter, k, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return (int) low;
+    }
+
+    private boolean canPlace(long[] pos, long perimeter, int k, long d) {
+        int n = pos.length;
+
+        long[] arr = new long[2 * n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = pos[i];
+            arr[i + n] = pos[i] + perimeter;
+        }
+
+        int[] next = new int[2 * n];
+        int j = 0;
+
+        for (int i = 0; i < 2 * n; i++) {
+            if (j < i + 1) {
+                j = i + 1;
+            }
+
+            while (j < 2 * n && arr[j] - arr[i] < d) {
+                j++;
+            }
+
+            next[i] = j;
+        }
+
+        for (int start = 0; start < n; start++) {
+            int current = start;
+
+            for (int count = 1; count < k; count++) {
+                current = next[current];
+
+                if (current >= start + n) {
+                    break;
+                }
+            }
+
+            if (current < start + n &&
+                arr[current] - arr[start] <= perimeter - d) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
